c2c1a3279560dd9e163a7676e5ddc52fe984723b
Svitlana <93516763+Svitlanka-K@users.noreply.github.com>
2022-08-10 21:06:18 +0300

    feat: task solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let x = '';
+   let absN = Math.abs( n );
+   let arr = ( "" + absN ).split( "" ).map( Number );
   
+  let newArr = arr.reverse();
+ 
+  x = newArr.join('');
+    
+  return parseInt(x, 10);
 }
